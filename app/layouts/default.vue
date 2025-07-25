471507c32d601257230ae9c5ebdd028b1491361f
difaMuhamad <difamuhamad06@gmail.com>
2025-07-26 06:58:27 +0700

    style: add shadcn-vue as components

---
+++ app/layouts/default.vue
@@ -0,0 +1,9 @@
+<template lang="">
+  <div class="container ini-template">
+    <slot />
+  </div>
+</template>
+<script>
+export default {};
+</script>
+<style lang=""></style>
---
+++ app/pages/index.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="flex justify-center content-center">
+    <Button>Click me</Button>
+  </div>
+</template>
+
+<script setup></script>
+
+<style lang=""></style>
